668469be1b2fe0747d34fc247e5429ddc5566241
nacho <nacho739@hotmail.com>
2022-07-13 13:30:47 -0500

    correcion poliza 2

--- resources/views/clientes/polizaD2.blade.php
+++ resources/views/clientes/polizaD2.blade.php
@@ -5,12 +5,12 @@
 <table style="font-size: 14px;">
   <tr>
     <th style="position: absolute;left: 35%; top: 17.8%"><b>{{$contrato->poliza}}</b></th>
-    <th style="position: absolute;left: 12%; top: 21.5%"><b>{{$contrato->contrato->nombres}} {{$contrato->contrato->apellidos}}</b></th>
+    <th style="position: absolute;left: 12%; top: 21%"><b>{{$contrato->contrato->nombres}} {{$contrato->contrato->apellidos}}</b></th>
     <th></th>
   </tr>
   <tr>
-    <td style="position: absolute;left: 14%;top: 24.3%;"><b>{{$direccion->calle}}</b></td>
-    <th style="position: absolute;left: 77%;top: 24.3%"><b>{{$contrato->contrato->telefono}}</b></th>
+    <td style="position: absolute;left: 14%;top: 24%;"><b>{{$direccion->calle}}</b></td>
+    <th style="position: absolute;left: 77%;top: 24%"><b>{{$contrato->contrato->telefono}}</b></th>
   </tr>
   <tr>
     <td style="position: absolute;left: 17%;top: 27%"><b>{{$direccion->cruzes}}</b></td>
@@ -47,20 +47,20 @@
   </tr>
 
     <tr>
-    <td style="position: absolute;left: 24%;top: 56%;"><b>{{money_format('%n', $pagos->costoservicio)}}</b></td>
+    <td style="position: absolute;left: 24%;top: 55%;"><b>{{money_format('%n', $pagos->costoservicio)}}</b></td>
     @if($contrato->plazo == '0.1')
-      <td style="position: absolute;left: 52%;top: 56%;"><b>{{date("d/m/Y", strtotime($contrato->desde))}}</b></td>
+      <td style="position: absolute;left: 52%;top: 55%;"><b>{{date("d/m/Y", strtotime($contrato->desde))}}</b></td>
     @else
-    <td style="position: absolute;left: 52%;top: 56%;"><b>{{date("d/m/Y", strtotime($pagos->pagoinicial))}}</b></td>
+    <td style="position: absolute;left: 52%;top: 55%;"><b>{{date("d/m/Y", strtotime($pagos->pagoinicial))}}</b></td>
     @endif
-    <td style="position: absolute;left: 83%;top: 56%;"><b>{{$pagos->forma_pago}}</b></td>
+    <td style="position: absolute;left: 84%;top: 55%;"><b>{{$pagos->forma_pago}}</b></td>
     
     <td style="position: absolute;left: 35%;top: 58%;"><b>@if($pagos->numeropagos == 4)4 pagos de $870 @elseif($pagos->numeropagos == 1) 1 pago de $3,480 @elseif($pagos->numeropagos == 3) 3 pagos de $1,160  @else @endif</b></td>
   </tr>
   <tr>
-    <td style="position: absolute;left: 14%;top: 90%;"><b>@if($contrato->plazo == '0.5')6 MESES @elseif($contrato->plazo == '0.1')1 MES @else {{$contrato->plazo}} AÑOS @endif</b></td>
-    <td style="position: absolute;left: 45%;top: 90%;"><b>{{ date("d/m/Y", strtotime($contrato->desde))}}</b></td>
-    <td style="position: absolute;left: 78%;top: 90%;"><b>{{ date("d/m/Y", strtotime($contrato->hasta))}}</b></td>
+    <td style="position: absolute;left: 14%;top: 89%;"><b>@if($contrato->plazo == '0.5')6 MESES @elseif($contrato->plazo == '0.1')1 MES @else {{$contrato->plazo}} AÑOS @endif</b></td>
+    <td style="position: absolute;left: 45%;top: 89%;"><b>{{ date("d/m/Y", strtotime($contrato->desde))}}</b></td>
+    <td style="position: absolute;left: 78%;top: 89%;"><b>{{ date("d/m/Y", strtotime($contrato->hasta))}}</b></td>
   </tr>
 </table>
 
